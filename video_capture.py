a8704ef58b382d75a816dc6bd8c5172164cbcc99
kristin-7 <dingchuanhu@ucsb.edu>
2023-04-12 15:35:16 -0700

    Finish video_capture

--- video_capture.py
+++ video_capture.py
@@ -1,2 +1,29 @@
-import numpy as np
-import cv2 as cv
+import cv2
+
+def main():
+
+    # Create a VideoCapture object
+    cap = cv2.VideoCapture(0)
+
+    # Create a VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('output.avi', fourcc, 24.0, (640, 480))
+
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        vidout = cv2.resize(frame, (640, 480))
+        out.write(vidout)
+        cv2.imshow('frame', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
